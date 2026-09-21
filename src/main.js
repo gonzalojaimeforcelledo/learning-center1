cd20519729b7b92cc4ee7be06b56b27dfb224137
gonzalo <gonzalo.jaimeforcelledo@gmail.com>
2026-09-21 11:03:24 -0500

    feat(enviroment):  add main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import PrimeVue from "primevue/config";
+import Material from "@primeuix/themes/material";
+import "primeflex/primeflex.css";
+import "primeicons/primeicons.css";
+import Tooltip from "primevue/tooltip";
+import {useTheme} from "@primeuix/themes";
 
-createApp(App).mount('#app')
+const primeVueUiLicenseKey=import.meta.env.VITE_PRIME_UI_LICENSE_KEY;
+
+createApp(App)
+    .use(PrimeVue the:{preset:Material}, ripple:true, license:PrimeUiLicenseKey);
+    .mount('#app')
