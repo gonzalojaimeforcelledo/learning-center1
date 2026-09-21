0ee1ae86788422fd1df1a11605614cc52b76c738
gonzalo <gonzalo.jaimeforcelledo@gmail.com>
2026-09-21 11:33:04 -0500

    feat(about): add about

---
+++ src/share/presentation/views/about.vue
@@ -0,0 +1,14 @@
+<script setup>
+import {useI18n} from 'vue-i18n';
+const {t} = useI18n();
+</script>
+
+<template>
+  <section>
+    <div class="flex flex-column gap-3">
+      <h1 class="font-bold text-xl">
+        {{t('about.title')}}
+      </h1>
+    </div>
+  </section>
+</template>
